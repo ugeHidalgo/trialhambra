b92987e12ec99a3b3ed33eae3e46d469245224cd
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-02-09 20:01:09 +0100

    Added initial config fro client side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalsService } from './globals/globals.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-
+// App modules
+import { AppRoutingModule } from './app-modules/app-routing.module';
 import { AppComponent } from './app.component';
 
+// Services
+import { GlobalsService } from './globals/globals.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    GlobalsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
