fe356cccf3ac0f739c269a54f8045b047e1a0fd8
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-02-10 08:38:57 +0100

    Added guards and toastr.

---
+++ client/src/app/animations.ts
@@ -0,0 +1,19 @@
+import { animate, AnimationEntryMetadata, state, style, transition, trigger } from '@angular/core';
+
+// Component transition animations
+export const slideInDownAnimation: AnimationEntryMetadata =
+  trigger('routeAnimation', [
+    state('*',
+      style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })
+    ),
+    transition(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateY(-100%)'
+      }),
+      animate('0.5s ease-in')
+    ])
+  ]);
