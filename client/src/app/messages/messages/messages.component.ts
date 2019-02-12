6d6edcce78d231faf5a96813847ece050087e4e5
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-02-12 20:58:02 +0100

    Added logic to message component and message service.

--- client/src/app/messages/messages/messages.component.ts
+++ client/src/app/messages/messages/messages.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessageService } from '../../services/message/message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
+
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
+
+  count: number;
 
   ngOnInit() {
+    const me = this;
+
+    me.count = 5;
+    me.countDowmBeforeClearMessages();
+  }
+
+  countDowmBeforeClearMessages(): void {
+    const me = this;
+
+    me.count--;
+    if (me.count <= 0) {
+      me.messageService.clear();
+      me.count = 5;
+      return;
+    }
+    setTimeout(() => {
+      me.countDowmBeforeClearMessages();
+    }, 1000);
   }
 
 }
--- client/src/app/services/message/message.service.ts
+++ client/src/app/services/message/message.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
+  messages: string[] = [];
+
   constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
