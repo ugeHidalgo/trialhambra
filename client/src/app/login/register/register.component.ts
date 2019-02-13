d244f435201664a1142c9f2b597b37101d9f31a2
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-02-13 22:52:29 +0100

    Added login, register and user components

--- client/src/app/login/register/register.component.ts
+++ client/src/app/login/register/register.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  loading = false;
+  validatingForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    public toastr: ToastrService,
+    private fb: FormBuilder,
+    vcr: ViewContainerRef ) {
+    this.createForm();
+ }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.validatingForm = this.fb.group({
+      username: [ '', Validators.required ],
+      firstname: '',
+      lastname: '',
+      password: '',
+      email: [ '', Validators.email ]
+    });
+  }
+
+  register() {
+    const me = this;
+
+    me.loading = true;
+    me.userService.registerUser(me.user)
+      .subscribe(
+        newUserAdded => {
+          me.toastr.success(`User ${newUserAdded.username} was successfully added.`);
+          me.router.navigate(['/login']);
+        },
+        error => {
+          me.toastr.error(error.error);
+          me.loading = false;
+        }
+      );
+  }
 }
