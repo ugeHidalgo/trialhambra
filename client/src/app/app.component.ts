83f378ea84788b79bbab87fc6d3f2e9b76bb618f
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-02-16 20:24:00 +0100

    Setting toast gloabal options

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,25 +11,12 @@ import { GlobalsService } from './globals/globals.service';
 
 export class AppComponent {
   title = 'TriAlhambra App';
-  //topMenuData: any;
 
   constructor ( protected globals: GlobalsService, private router: Router ) {
     const me = this,
           username = me.globals.getUserNameFromLocalStorage();
 
     me.globals.setUser(username);
-
-    /* this.topMenuData = JSON.parse(`[{
-        "text": "File",
-        "submenus": [{
-          "text": "Accounts",
-          "routerLink": "/accounts"
-        }, {
-          "text": "Cost centres",
-          "routerLink": "/costcentres"
-        }]
-      }]
-    `); */
   }
 
   onClick(link) {
--- client/src/app/messages/toastr/toastrCustomOptions.ts
+++ client/src/app/messages/toastr/toastrCustomOptions.ts
@@ -1,11 +1,8 @@
-import { ToastrConfig } from 'ngx-toastr';
-
 export class ToastrCustomOptions  {
-  toastLife = 2000;
-  positionClass = 'toast-top-center';
-  animate = 'flyLeft';
-  newestOnTop = false;
-  showCloseButton = true;
-  titleClass = 'toastrCustomOptions.scss';
+  timeOut: 2000;
+  positionClass: 'toast-top-center';
+  preventDuplicates: true;
+  tapToDissmiss: true;
+  closeButton: true;
 }
 
